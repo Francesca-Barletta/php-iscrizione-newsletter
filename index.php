7f84e45aa37af8c25ed0176cfaf7ba5e1377a82a
Francesca Barletta <wallebar@gmail.com>
2024-05-07 19:22:12 +0200

    add alert in bootstrap

--- index.php
+++ index.php
@@ -3,17 +3,21 @@
 $email = $_POST['email'] ?? '';
 var_dump("email: ,$email");
 var_dump(isset($_POST['email']));
-
+// clicco invia e $_POST['email'] diventa true, a quel punto verifico la mail e escono gli alert di conseguenza,
+//se ancora non ho cliccato e $_POST['email'] è ancora falso non devo vedere nessun alert
 function validEmail($email){
-    //se il pulsante invia è stato cliccato, la mail è lunga più di 5 e contiene sia '@' che '.' allora la email è valida (true)
-    if(strlen($email) > 5 && str_contains($email,'@') && str_contains($email, '.')){
-        var_dump("la mail $email è valida");
-        return true;
-        //se la il pulsante invia è stato cliccato e la mail è vuota allora inserisci email (false)    
-    }elseif(isset($_POST['email'])){
-        var_dump("la mail $email non è valida");
-        return false;
+    if(isset($_POST['email']) === true){
+        //se il pulsante invia è stato cliccato, la mail è lunga più di 5 e contiene sia '@' che '.' allora la email è valida (true)
+            if(strlen($email) > 5 && str_contains($email,'@') && str_contains($email, '.')){
+                // var_dump("la mail $email è valida");
+                return true;
+                //se la il pulsante invia è stato cliccato e la mail è vuota allora inserisci email (false)    
+         }else{
+                // var_dump("la mail $email non è valida");
+                return false;
+         };
     };
+    
 };
 var_dump(validEmail($email));
 
@@ -44,10 +48,21 @@ var_dump(validEmail($email));
             <div>
                 <button>invia</button>
             </div>
+
         </form>
-        </div>
+        <div class>
+                <?php if(validEmail($email) === true) { ?>
+                    <div class="alert alert-primary col-4" role="alert">
+                    email valida
+                  </div>
+                <?php }elseif(validEmail($email) === false){ ?>
+                    <div class="alert alert-danger col-4" role="alert">
+                    email non valida
+                  </div>
+                 <?php } ?>
+            </div>
     </div>
    
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>  
 </body>
 </html>

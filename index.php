f8a26463577bcfd9697e02011fde865f42a780fc
Francesca Barletta <wallebar@gmail.com>
2024-05-07 15:11:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <form action="" method="POST">
+          <div class="mb-3 col-4" >
+            <label for="email" class="form-label">Iscriviti alla newsletter:</label>
+            <input type="text" class="form-control" id="email" placeholder="example@gmail.com">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    </div>
+   
+    
+</body>
+</html>
